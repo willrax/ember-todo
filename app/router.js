0610003caf0111ef90d6abe97f4e59bc4443b3a4
Will Raxworthy <me@willrax.com>
2015-11-10 14:57:11 -0500

    initial

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('todos', function() {
+    this.route('todo', { path: "/:id" })
+  })
 });
 
 export default Router;
